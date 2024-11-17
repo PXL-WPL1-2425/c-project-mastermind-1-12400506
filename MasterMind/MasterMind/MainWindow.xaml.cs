7dbd2962c595eb04ad9363841638d0309e053eb2
Kobe Kerkhofs <12400506@student.pxl.be>
2024-11-17 17:09:20 +0100

    MasterMind-04

--- MasterMind/MasterMind/MainWindow.xaml.cs
+++ MasterMind/MasterMind/MainWindow.xaml.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Security.Cryptography;
-using System.Text;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MasterMind
 {
@@ -17,11 +10,12 @@ namespace MasterMind
     {
         private string[] allColors = { "Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw" };
         private string[] code;
+
         public MainWindow()
         {
             InitializeComponent();
             InitialiseerComboBoxes();
-            generateSecretCode();
+            GenerateSecretCode();
         }
 
         private void InitialiseerComboBoxes()
@@ -32,17 +26,60 @@ namespace MasterMind
             comboBox4.ItemsSource = allColors;
         }
 
-
-        private void generateSecretCode()
+        private void GenerateSecretCode()
         {
             Random number = new Random();
             code = Enumerable.Range(0, 4)
-                                    .Select(_ => allColors[number.Next(allColors.Length)])
-                                    .ToArray();
+                             .Select(_ => allColors[number.Next(allColors.Length)])
+                             .ToArray();
 
-            // Toon de geheime code in het venstertitel (voor testen)
-            this.Title = $"secret Code: {string.Join(", ", code)}";
+            this.Title = $"Secret Code: {string.Join(", ", code)}";
         }
 
-}
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender == comboBox1)
+                UpdateLabelColor(label1, comboBox1.SelectedItem?.ToString());
+            else if (sender == comboBox2)
+                UpdateLabelColor(label2, comboBox2.SelectedItem?.ToString());
+            else if (sender == comboBox3)
+                UpdateLabelColor(label3, comboBox3.SelectedItem?.ToString());
+            else if (sender == comboBox4)
+                UpdateLabelColor(label4, comboBox4.SelectedItem?.ToString());
+        }
+
+        private void UpdateLabelColor(Label label, string colorName)
+        {
+            if (colorName == null)
+            {
+                label.Background = Brushes.Transparent;
+                return;
+            }
+
+            switch (colorName.ToLower())
+            {
+                case "rood":
+                    label.Background = Brushes.Red;
+                    break;
+                case "geel":
+                    label.Background = Brushes.Yellow;
+                    break;
+                case "oranje":
+                    label.Background = Brushes.Orange;
+                    break;
+                case "wit":
+                    label.Background = Brushes.White;
+                    break;
+                case "groen":
+                    label.Background = Brushes.Green;
+                    break;
+                case "blauw":
+                    label.Background = Brushes.Blue;
+                    break;
+                default:
+                    label.Background = Brushes.Transparent;
+                    break;
+            }
+        }
+    }
 }
