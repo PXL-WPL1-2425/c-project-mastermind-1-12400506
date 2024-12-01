95bec17bce559d86ef21ee8d97a7860aba6725e3
12400506 <12400506@student.pxl.be>
2024-12-01 18:06:26 +0100

    Mastermind-06

--- MasterMind/MasterMind/MainWindow.xaml.cs
+++ MasterMind/MasterMind/MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace MasterMind
 {
@@ -10,12 +12,17 @@ namespace MasterMind
     {
         private string[] allColors = { "Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw" };
         private string[] code;
+        private int attempts = 0;
+        private const int maxAttemps = 10;
+        private DispatcherTimer timer;
+        private bool isDebugMode = false;
 
         public MainWindow()
         {
             InitializeComponent();
             InitialiseerComboBoxes();
             GenerateSecretCode();
+            InitializeTimer();
         }
 
         private void InitialiseerComboBoxes()
@@ -33,8 +40,12 @@ namespace MasterMind
                              .Select(_ => allColors[number.Next(allColors.Length)])
                              .ToArray();
 
-            this.Title = $"Secret Code: {string.Join(", ", code)}"; // Alleen voor debug/testdoeleinden
+            attempts = 0;
+            UpdateTitle();
+            ResetLabelBorders();
+            this.Title = $"Secret Code: {string.Join(", ", code)}"; 
         }
+
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -91,8 +102,8 @@ namespace MasterMind
                 comboBox4.SelectedItem?.ToString()
             };
 
-            ResetLabelBorders();
 
+            ResetLabelBorders();
             for (int i = 0; i < selectedColors.Length; i++)
             {
                 if (selectedColors[i] == code[i])
@@ -104,7 +115,43 @@ namespace MasterMind
                     GetLabelByIndex(i).BorderBrush = Brushes.Wheat;
                 }
             }
+
+            if (selectedColors.Any(c => c == null))
+            {
+                MessageBox.Show("Vul alle kleuren in voordat je de code controleert.", "Waarschuwing", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            attempts++;
+            UpdateTitle();
+
+            if (IsCodeCracked(selectedColors))
+            {
+                EndGame(true);
+            }
+            else if (attempts >= maxAttemps)
+            {
+                EndGame(false);
+            }
         }
+
+        private bool IsCodeCracked(string[] selectedColors)
+        {
+            return selectedColors.SequenceEqual(code);
+        }
+
+        private void EndGame(bool isWinner)
+        {
+            string message = isWinner
+                ? $"Gefeliciteerd! Je hebt de code gekraakt in {attempts} pogingen!"
+                : $"Helaas, je hebt de code niet gekraakt. De geheime code was: {string.Join(", ", code)}.";
+
+            MessageBox.Show(message, "Spel Einde", MessageBoxButton.OK, MessageBoxImage.Information);
+            
+            Application.Current.Shutdown();
+        }
+
+
 
         private void ResetLabelBorders()
         {
@@ -112,6 +159,10 @@ namespace MasterMind
             label2.BorderBrush = Brushes.Black;
             label3.BorderBrush = Brushes.Black;
             label4.BorderBrush = Brushes.Black;
+        }
+        private void UpdateTitle()
+        {
+            this.Title = $"Poging {attempts}/{maxAttemps}";
         }
 
         private Label GetLabelByIndex(int index)
@@ -124,6 +175,47 @@ namespace MasterMind
                 3 => label4,
                 _ => throw new ArgumentOutOfRangeException(nameof(index))
             };
+        }
+
+
+        private void ToggleDebug()
+        {
+            isDebugMode = !isDebugMode;
+            debugTextBox.Visibility = isDebugMode ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(10)
+            };
+            timer.Tick += Timer_Tick;
+        }
+
+        private void StartCountdown()
+        {
+            timer.Stop();
+            timer.Start();
+        }
+        private void StopCountdown()
+        {
+            timer.Stop();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Je hebt te lang gewacht! Je beurt is voorbij.", "Tijd verstreken", MessageBoxButton.OK, MessageBoxImage.Warning);
+            attempts++;
+            UpdateTitle();
+        }
+
+        private void DebugShortcut(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F12 && Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                ToggleDebug();
+            }
         }
     }
 }
